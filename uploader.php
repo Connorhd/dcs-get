a1b9f1af6ab89679de4a60c99c26757b0ce3e950
mrwilson <csuiav@warwick.ac.uk>
2010-11-01 20:23:10 +0800

    Added 'upload' to dcs-get script. Tidied uploader.php. Added form on index.html.

--- uploader.php
+++ uploader.php
@@ -1,21 +1,24 @@
 <?php 
 $upload_directory=dirname(__FILE__).'/uploads/';
-//check if form submitted
+ini_set("post_max_size", "30M");
+ini_set("upload_max_filesize", "30M");
+ini_set("memory_limit", "40M");
+
 if (isset($_POST['upload'])) {  
-    if (!empty($_FILES['my_file'])) { 
-			//check for image submitted
-    		if ($_FILES['my_file']['error'] > 0) { 
-			// check for error re file
-            echo "Error: " . $_FILES["my_file"]["error"] ;
+    if (!empty($_FILES['foo'])) { 
+    		if ($_FILES['foo']['error'] > 0) { 
+            echo "Error: " . $_FILES["foo"]["error"]."\n" ;
         } else {
-			//move temp file to our server            
-			move_uploaded_file($_FILES['my_file']['tmp_name'], 
-			$upload_directory . $_FILES['my_file']['name']);	
-			echo 'Uploaded File. :D';
+			move_uploaded_file($_FILES['foo']['tmp_name'], 
+			$upload_directory . $_FILES['foo']['name']);	
+			echo 'Uploaded File.'."\n";
         }
     } else {
-	        die('File not uploaded. D:'); 
+	        echo "Massive Fail.\n";
+		die('File not uploaded.'); 
 			// exit script
     }
 }
+else 
+{ echo 'Something broke. Hurt the person responsible.'."\n";}
 ?>
