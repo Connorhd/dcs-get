55f66c8ea198e440027f3d8d26fc060ee58272b4
Connor Dunn <connorhd@gmail.com>
2010-11-01 13:44:45 +0000

    Tidied up code.

--- uploader.php
+++ uploader.php
@@ -1,24 +1,24 @@
 <?php 
-$upload_directory=dirname(__FILE__).'/uploads/';
-ini_set("post_max_size", "30M");
-ini_set("upload_max_filesize", "30M");
-ini_set("memory_limit", "40M");
 
-if (isset($_POST['upload'])) {  
-    if (!empty($_FILES['foo'])) { 
-    		if ($_FILES['foo']['error'] > 0) { 
-            echo "Error: " . $_FILES["foo"]["error"]."\n" ;
-        } else {
+$upload_directory=dirname(__FILE__).'/uploads/';
+ini_set("post_max_size", "250M");
+ini_set("upload_max_filesize", "250M");
+ini_set("memory_limit", "300M");
+
+if (isset($_POST['upload'])) {
+	if (!empty($_FILES['foo'])) {
+		if ($_FILES['foo']['error'] > 0) {
+			echo "Error: " . $_FILES["foo"]["error"]."\n";
+		} else {
 			move_uploaded_file($_FILES['foo']['tmp_name'], 
 			$upload_directory . $_FILES['foo']['name']);	
 			echo 'Uploaded File.'."\n";
-        }
-    } else {
-	        echo "Massive Fail.\n";
+		}
+	} else {
+		echo "Massive Fail.\n";
 		die('File not uploaded.'); 
-			// exit script
-    }
+		// exit script
+	}
+} else {
+	echo 'Something broke. Hurt the person responsible.'."\n";
 }
-else 
-{ echo 'Something broke. Hurt the person responsible.'."\n";}
-?>
