d89819f96a5877ebadd77338d3b9b70ad17f45b3
Chris Sinjakli <chris@sinjakli.co.uk>
2010-12-13 08:06:28 +0800

    Improve how variables are output. Remove useless php tag. Switch to associative arrays for decoded json.

--- package.php
+++ package.php
@@ -9,31 +9,26 @@
 <h2>Packages</h2>
 <?php
 
-error_reporting(0);
-//error_reporting(E_ALL);
+//error_reporting(0);
+error_reporting(E_ALL);
 
 define('BASE_URL', 'http://backus.uwcs.co.uk/dcs-get/');
 
-$packages = json_decode(file_get_contents(BASE_URL.'packages.json'));
+$packages = json_decode(file_get_contents(BASE_URL.'packages.json'), true);
+ksort($packages);
 
-$sorted_packages = array();
-foreach ($packages as $package => $data) {
-	$sorted_packages[$package] = $data;
-}
-ksort($sorted_packages);
-
-foreach($sorted_packages as $package => $data) {
+foreach($packages as $package => $data) {
 	echo '<ul>';
 	echo '<li>';
-	echo "$package - ";
-	if (isset($data->description)) {
-		echo "$data->description - ";
+	echo htmlspecialchars($package).' - ';
+	if (isset($data['description'])) {
+		echo htmlspecialchars($data['description']).' - ';
 	}
-	echo 'Version: '.implode(', ', $data->version).'<br>';
-	if (isset($data->dependencies)) {
+	echo 'Version: '.htmlspecialchars(implode(', ', $data['version'])).'<br>';
+	if (isset($data['dependencies'])) {
 		echo '<ul>';
-		foreach ($data->dependencies as $dep => $stuff) {
-			echo "<li>$stuff[0] - $stuff[1]</li>";
+		foreach ($data['dependencies'] as $dep => $details) {
+			echo '<li>'.htmlspecialchars($details[0]).' - '.htmlspecialchars($details[1]).'</li>';
 		}
 		echo '</ul>';
 	}
@@ -43,4 +38,3 @@ foreach($sorted_packages as $package => $data) {
 ?>
 </body>
 </html>
-<?php
